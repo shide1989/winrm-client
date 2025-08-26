d1fb754319c4007281440009d85cb30dff8ccfd7
Sebastien Hideux <hideux.sebastien@gmail.com>
2025-08-26 22:46:54 +0200

    feat: added comment

--- tests/interactive.test.ts
+++ tests/interactive.test.ts
@@ -7,6 +7,7 @@ if (!JEST_WINRM_PASS || !JEST_WINRM_USER || !JEST_WINRM_HOST) {
 }
 
 describe('interactive', () => {
+  // TODO: This test is not working. It will need improvement since its not a real interactive command.
   it('should execute a powershell command with an interactive command', async () => {
     const result = await runPowershell(
       'Write-Host \\"Hello, World!\\"',
